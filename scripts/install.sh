7c06cf213ed5d5413ec6645534b0741ce9fa1fb1
mags0ft <magnus@schlinsog.eu>
2025-08-11 23:12:58 +0200

    fix wrong install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,13 +7,17 @@ if ! command -v pyenv &> /dev/null; then
 fi
 
 # zuerst die Dependencies fürs Training installieren
-pyenv install 3.8.10 && pyenv global 3.8.10 && python3 -m venv .venv-training
+pyenv install 3.8.10
+pyenv global 3.8.10
+python3 -m venv .venv-training
 source .venv-training/bin/activate
 pip install --no-deps -r ./requirements-training.txt
 deactivate
 
 # jetzt kommen die für unser GUI
-pyenv install 3.10.12 && pyenv global 3.10.12 && python3 -m venv .venv-server
+pyenv install 3.10.12
+pyenv global 3.10.12
+python3 -m venv .venv-server
 source .venv-server/bin/activate
 pip install -r ./requirements-server.txt
 
